bad53714f471606f11db289b1ae0dd1c00c3aaa9
Vinicius Vieira <viniciusvsoares00@gmail.com>
2023-06-15 20:15:04 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-window.addEventListener('scroll', function () {
+/*window.addEventListener('scroll', function () {
     let header = document.querySelector('.navbar');
     let icon = document.querySelector('.fa-house');
     header.classList.toggle('sticky', window.scrollY > 0);
     icon.classList.toggle('sticky', window.scrollY > 0);
 });
 
-/*function toggleMenu() {
+function toggleMenu() {
     var menuToggle = document.querySelector('.toggle');
     var menuToggle = document.querySelector('.navbar-nav');
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active')
 }
 */
+
+const typingText = document.getElementById('typing-text');
+
+function startTypingAnimation() {
+    typingText.style.animation = 'typing 3s steps(40, end), blink-caret .75s step-end infinite';
+}
+
+typingText.addEventListener('animationend', () => {
+    typingText.style.animation = 'none';
+    setTimeout(startTypingAnimation, 100);
+});
+
+startTypingAnimation();
